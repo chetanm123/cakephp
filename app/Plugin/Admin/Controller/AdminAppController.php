8486eae1a44d4ae08b2a1021790d3dc8bb08a45a
Chetan <chetan@vouchercodesuae.com>
2014-08-22 17:21:02 +0530

    plugin for admin development

--- app/Plugin/Admin/Controller/AdminAppController.php
+++ app/Plugin/Admin/Controller/AdminAppController.php
@@ -1,16 +1,43 @@
 <?php
 class AdminAppController extends AppController
 {
-	public $components=array(
+	public $components = array(
 		'Session',
 		'Auth'=>array(
-				'authorize'=>array('Plugin')
-			)
+				'loginRedirect'=>array(
+					'controller'=>'posts',
+					'action'=>'index'
+				),
+				'logoutRedirect'=>array(
+					'controller'=>'pages',
+					'action'=>'display',
+					'home'
+				),
+				'authorize'=>array('Controller'),
+				'authenticate'=>array(
+					'Form'=>array(
+						'passwordHasher'=>'Blowfish'
+					)
+				)
+			),
+		'Cookie'
 		);
-	
+
+	public $allowedPlugins=array();
+
 	public function beforeFilter(){
 		parent::beforeFilter();
-		$this->Auth->allow('index');
+		$this->Auth->allow('index','view');
+		$this->set('logged_in',$this->Auth->login());
+		$this->set('current_user',$this->Auth->user());
+	}
+
+	public function isAuthorized($user){
+		//Admin can access every action
+		if(isset($user['role']) && $user['role']==='admin'){
+			return true;
+		}
+		return false;
 	}
 }
 ?>
